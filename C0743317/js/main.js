92be8de2b7b670d3dcdae907abe334f8b3b8e615
CSD2214_SECTION5_WEEK_05 <visualstudio@230-PC11>
2019-02-07 11:55:48 -0500

    final commit

--- C0743317/js/main.js
+++ C0743317/js/main.js
@@ -1,11 +1,19 @@
 ﻿// Your code here!
 function myFunction(a, b) {
-    var a = 5;
-    var b = 6;
+    //var a = 5;
+    //var b = 6;
     return a + b; //function returns the product of p1 and p2
 }
 function arrays() {
     var pens;
     pens = ["red", "blue", "orange", "blue", "green"];
-    console.log(pens);
+    marker = pens;
+    console.log(marker);
+    console.log(marker[0]);
+
+    for (i = 0; i < marker.length; i++) {
+        alert(marker[i]);
+    }
+    alert("Length of array is:" + marker.length);
 }
+
